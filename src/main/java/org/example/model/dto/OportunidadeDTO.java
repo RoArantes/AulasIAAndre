18ced1544d91fdb04dc62df1d0dffdb1dd982c8f
Roberta Arantes <betaarantes@hotmail.com>
2025-03-23 21:17:26 -0300

    Add files via upload

---
+++ src/main/java/org/example/model/dto/OportunidadeDTO.java
@@ -0,0 +1,32 @@
+package org.example.model.dto;
+
+public class OportunidadeDTO {
+
+    private String description;
+    private String data;
+    private String satisfacao;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getSatisfacao() {
+        return satisfacao;
+    }
+
+    public void setSatisfacao(String satisfacao) {
+        this.satisfacao = satisfacao;
+    }
+}
